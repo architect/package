b0fedf8e821f8053d04501ed6fb77c67332c5d5d
filmaj <maj.fil@gmail.com>
2019-07-10 14:57:49 -0500

    ✅🔪⚙️ adding unit tests, removing unused packages, adding ci

--- test/mock/src/scheduled/daily/index.js
+++
@@ -1,5 +0,0 @@
-// learn more about scheduled functions here: https://arc.codes/reference/scheduled
-exports.handler = async function subscribe(payload) {
-  console.log(JSON.stringify(payload, null, 2))
-  return
-}
