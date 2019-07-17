3d6745f78840fddcfcb98d97464b1f047d59692a
Brian LeRoux <b@brian.io>
2019-07-16 20:20:49 -0700

    adds workflow for `npm run slo:test` (maybe while drinking slo gin!)

--- test/mock/src/http/post-index/index.js
+++
@@ -1,7 +0,0 @@
-// learn more about http functions here: https://arc.codes/primitives/http
-exports.handler = async function http() {
-  return {
-    headers: {'content-type': 'text/html; charset=utf8'},
-    body: '<b>hello world</b> from nodejs'
-  }
-}
---
+++ test/slow/integration-test.js
@@ -0,0 +1,78 @@
+let parse = require('@architect/parser')
+let test = require('tape')
+let child = require('child_process')
+let path = require('path')
+let fs = require('fs')
+let pkg = require('../../')
+
+// run package
+// run deploy
+let arcfile = `
+@app
+test-pkg
+
+@static
+@http
+get /api
+`
+
+test('write sam.json', t=> {
+  t.plan(1)
+  let sam = pkg.toSAM(parse(arcfile))
+  fs.writeFileSync(__dirname + '/mock/sam.json', JSON.stringify(sam, null, 2))
+  t.ok(true, 'ran')
+  console.log(sam)
+})
+
+// helper function for child.spawn
+function spawn(command, args, callback) {
+  let cwd = path.join(__dirname, 'mock')
+  let pkg = child.spawn(command, args, {cwd, shell: true})
+  pkg.stdout.on('data', b=> console.log(b.toString()))
+  pkg.stderr.on('data', function(b) {
+    console.log(b.toString())
+    callback(Error('stderr'))
+  })
+  pkg.on('close', ()=> callback())
+  pkg.on('error', callback)
+}
+
+test('sam package', t=> {
+  t.plan(1)
+  spawn('sam', [
+    'package',
+    '--template-file',
+    `sam.json`,
+    '--output-template-file',
+    `sam.yaml`,
+    '--s3-bucket',
+    'cf-sam-deployments-east',
+  ],
+  function done(err) {
+    if (err) t.fail(err)
+    else {
+      t.ok(true, 'packaged')
+    }
+  })
+})
+
+test('deploy', t=> {
+  t.plan(1)
+  spawn('sam', [
+    'deploy',
+    '--template-file',
+    `sam.yaml`,
+    '--stack-name',
+    'PackageTestApp',
+    '--s3-bucket',
+    'cf-sam-deployments-east',
+    '--capabilities',
+    'CAPABILITY_IAM'
+  ],
+  function done(err) {
+    if (err) t.fail(err)
+    else {
+      t.ok(true, 'deployed')
+    }
+  })
+})
