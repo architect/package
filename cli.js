9a58a63cf7d6d1b6f08f411bcb9646e6abdda489
Brian LeRoux <b@brian.io>
2019-08-12 12:37:14 -0700

    moves cli back in

---
+++ cli.js
@@ -0,0 +1,28 @@
+let readArcFile = require('@architect/utils/read-arc')
+let config = require('.')
+let chalk = require('chalk')
+let fs = require('fs')
+
+module.exports = function pack(/*opts*/) {
+
+  let {arc} = readArcFile()
+  fs.writeFileSync('sam.json', JSON.stringify(config(arc), null, 2))
+
+  // draw the deploy instructions
+  let pkg = `sam package --template-file sam.json --output-template-file out.yaml --s3-bucket [S3 bucket]`
+  let dep = `sam deploy --template-file out.yaml --stack-name [Stack Name] --s3-bucket [S3 bucket] --capabilities CAPABILITY_IAM`
+  let sam = chalk.bold.green(pkg)
+  let cf = chalk.bold.green(dep)
+
+  let x = process.platform.startsWith('win')? ' √' :'✓'
+  let f = chalk.cyan.bold('sam.json')
+
+  console.log(chalk.grey(`${x} Successfully created ${f}! Now deploy it by following these steps:
+
+1.) Package code with SAM:
+${sam}
+
+2.) Deploy the CloudFormation stack:
+${cf}
+  `))
+}
