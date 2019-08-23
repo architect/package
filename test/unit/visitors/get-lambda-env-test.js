df7bab065c31d82b09ec76a0883bc3a857213cb2
Brian LeRoux <b@brian.io>
2019-08-23 14:28:22 -0700

    updates deps and removes old test

--- test/unit/visitors/get-lambda-env-test.js
+++ test/unit/visitors/get-lambda-env-test.js
@@ -1,7 +1,7 @@
 let test = require('tape')
 let getEnv = require('../../../src/visitors/get-lambda-env')
 let parser = require('@architect/parser')
-let sinon = require('sinon')
+//let sinon = require('sinon')
 let fs = require('fs')
 let path = require('path')
 let shortArcFile = fs.readFileSync(path.join(__dirname, '..', '.arc-short')).toString()
@@ -21,38 +21,3 @@ test('get-lambda-env: if static exists, sets ARC_STATIC_BUCKET', t => {
   t.deepEquals(result.ARC_STATIC_BUCKET, {Ref: 'StaticBucket'}, 'ARC_STATIC_BUCKET set')
 })
 
-test('get-lambda-env: if .arc-env file exists, subsumes environment variables present therein', t => {
-  t.plan(2)
-  let existsStub = sinon.stub(fs, 'existsSync').returns(true)
-  let readStub = sinon.stub(fs, 'readFileSync').returns({
-    toString() {return {
-      trim() { return `@testing
-GLOBAL asdfasdf
-
-@staging
-GLOBAL_KEY val`}
-    }}
-  })
-  let result = getEnv(ARC)
-  t.deepEquals(result.testing, [['GLOBAL', 'asdfasdf']], 'testing env var present')
-  t.deepEquals(result.staging, [['GLOBAL_KEY', 'val']], 'staging env var present')
-  existsStub.restore()
-  readStub.restore()
-})
-
-test('get-lambda-env: if .arc-env file exists, subsumes production environment variables present therein', t => {
-  t.plan(2)
-  let existsStub = sinon.stub(fs, 'existsSync').returns(true)
-  let readStub = sinon.stub(fs, 'readFileSync').returns({
-    toString() {return {
-      trim() { return `@production
-API_KEY 1234
-DB_PASSWORD 12345`}
-    }}
-  })
-  let result = getEnv(ARC)
-  t.equals(result.API_KEY, 1234, 'first production env var set correctly')
-  t.equals(result.DB_PASSWORD, 12345, 'second production env var set correctly')
-  existsStub.restore()
-  readStub.restore()
-})
