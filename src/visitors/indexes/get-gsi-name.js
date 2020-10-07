e072a77cfe4a210fef6f01c76b0b929ff59d90ef
Ian Thomas <ian.thomas@skybettingandgaming.com>
2020-10-07 13:18:27 +0100

    Adds support for named indexes

--- src/visitors/indexes/get-gsi-name.js
+++ src/visitors/indexes/get-gsi-name.js
@@ -1,8 +1,13 @@
 module.exports = function _getGsiName (index) {
-  let { name, partitionKey, partitionKeyType, sortKey } = index
+  let { indexName, name, partitionKey, partitionKeyType, sortKey } = index
   if (!partitionKey || !partitionKeyType) {
     throw Error(`Invalid @indexes: ${name}`)
   }
+
+  if (typeof indexName === 'string' && indexName.length > 0) {
+    return indexName
+  }
+
   let s = sortKey ? `-${sortKey}` : '' // Naming extension for multi-keys
   return `${partitionKey}${s}-index` // New school index naming
 }
