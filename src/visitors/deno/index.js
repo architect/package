e2d5a158491ffe6a9c04a63a7f7e62827fe1967f
Ryan Block <rblock@gmail.com>
2023-08-22 19:05:20 -0700

    Upgrade Deno to 1.36.2

--- src/visitors/deno/index.js
+++ src/visitors/deno/index.js
@@ -1,26 +1,26 @@
 /**
  * support for Deno custom runtime
+ * https://github.com/beginner-corp/begin-deno-runtime
  */
 module.exports = function visitDeno (inventory, template) {
-
   let deno = {
-    'us-east-1': 'arn:aws:lambda:us-east-1:455488262213:layer:DenoRuntime:28',
-    'us-east-2': 'arn:aws:lambda:us-east-2:455488262213:layer:DenoRuntime:19',
-    'us-west-1': 'arn:aws:lambda:us-west-1:455488262213:layer:DenoRuntime:19',
-    'us-west-2': 'arn:aws:lambda:us-west-2:455488262213:layer:DenoRuntime:22',
-    'ca-central-1': 'arn:aws:lambda:ca-central-1:455488262213:layer:DenoRuntime:65',
-    'ap-south-1': 'arn:aws:lambda:ap-south-1:455488262213:layer:DenoRuntime:2',
-    'ap-northeast-3': 'arn:aws:lambda:ap-northeast-3:455488262213:layer:DenoRuntime:1',
-    'ap-northeast-2': 'arn:aws:lambda:ap-northeast-2:455488262213:layer:DenoRuntime:1',
-    'ap-southeast-1': 'arn:aws:lambda:ap-southeast-1:455488262213:layer:DenoRuntime:2',
-    'ap-southeast-2': 'arn:aws:lambda:ap-southeast-2:455488262213:layer:DenoRuntime:2',
-    'ap-northeast-1': 'arn:aws:lambda:ap-northeast-1:455488262213:layer:DenoRuntime:2',
-    'eu-central-1': 'arn:aws:lambda:eu-central-1:455488262213:layer:DenoRuntime:2',
-    'eu-west-1': 'arn:aws:lambda:eu-west-1:455488262213:layer:DenoRuntime:2',
-    'eu-west-2': 'arn:aws:lambda:eu-west-2:455488262213:layer:DenoRuntime:2',
-    'eu-north-1': 'arn:aws:lambda:eu-north-1:455488262213:layer:DenoRuntime:1',
-    'eu-west-3': 'arn:aws:lambda:eu-west-3:455488262213:layer:DenoRuntime:1',
-    'sa-east-1': 'arn:aws:lambda:sa-east-1:455488262213:layer:DenoRuntime:1',
+    'ap-northeast-1': 'arn:aws:lambda:ap-northeast-1:455488262213:layer:DenoRuntime:16',
+    'ap-northeast-2': 'arn:aws:lambda:ap-northeast-2:455488262213:layer:DenoRuntime:15',
+    'ap-northeast-3': 'arn:aws:lambda:ap-northeast-3:455488262213:layer:DenoRuntime:15',
+    'ap-south-1':     'arn:aws:lambda:ap-south-1:455488262213:layer:DenoRuntime:16',
+    'ap-southeast-1': 'arn:aws:lambda:ap-southeast-1:455488262213:layer:DenoRuntime:16',
+    'ap-southeast-2': 'arn:aws:lambda:ap-southeast-2:455488262213:layer:DenoRuntime:16',
+    'ca-central-1':   'arn:aws:lambda:ca-central-1:455488262213:layer:DenoRuntime:79',
+    'eu-central-1':   'arn:aws:lambda:eu-central-1:455488262213:layer:DenoRuntime:16',
+    'eu-north-1':     'arn:aws:lambda:eu-north-1:455488262213:layer:DenoRuntime:15',
+    'eu-west-1':      'arn:aws:lambda:eu-west-1:455488262213:layer:DenoRuntime:16',
+    'eu-west-2':      'arn:aws:lambda:eu-west-2:455488262213:layer:DenoRuntime:16',
+    'eu-west-3':      'arn:aws:lambda:eu-west-3:455488262213:layer:DenoRuntime:15',
+    'sa-east-1':      'arn:aws:lambda:sa-east-1:455488262213:layer:DenoRuntime:15',
+    'us-east-1':      'arn:aws:lambda:us-east-1:455488262213:layer:DenoRuntime:42',
+    'us-east-2':      'arn:aws:lambda:us-east-2:455488262213:layer:DenoRuntime:33',
+    'us-west-1':      'arn:aws:lambda:us-west-1:455488262213:layer:DenoRuntime:33',
+    'us-west-2':      'arn:aws:lambda:us-west-2:455488262213:layer:DenoRuntime:36',
   }
 
   for (let resource of Object.keys(template.Resources)) {
