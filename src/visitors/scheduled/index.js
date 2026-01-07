79e54afd58f962f3ceac8e00f69c796886852784
Simon MacDonald <simon.macdonald@gmail.com>
2026-01-07 16:33:58 -0500

    feat: allow scheduled function to specify timezone
    Signed-off-by: Simon MacDonald <simon.macdonald@gmail.com>

--- src/visitors/scheduled/index.js
+++ src/visitors/scheduled/index.js
@@ -8,16 +8,15 @@ module.exports = function visitScheduled (inventory, template) {
   let { inv } = inventory
   if (!inv.scheduled) return template
 
-  // we leave the bucket name generation up to cloudfront
   inv.scheduled.forEach(schedule => {
-    let { rate, cron } = schedule
+    let { rate, cron, timezone } = schedule
     let rule = rate || cron
     rule = `${rate ? 'rate' : 'cron'}(${rule.expression})`
 
     let name = toLogicalID(schedule.name)
     let scheduleLambda = `${name}ScheduledLambda`
     let scheduleEvent = `${name}ScheduledEvent`
-    let schedulePermission = `${name}ScheduledPermission`
+    let scheduleRole = `${name}ScheduledRole`
 
     // Create the Lambda
     template.Resources[scheduleLambda] = createLambda({
@@ -26,29 +25,52 @@ module.exports = function visitScheduled (inventory, template) {
       template,
     })
 
-    // Create the scheduled event rule
-    template.Resources[scheduleEvent] = {
-      Type: 'AWS::Events::Rule',
+    // Create IAM role for EventBridge Scheduler to invoke the Lambda
+    template.Resources[scheduleRole] = {
+      Type: 'AWS::IAM::Role',
       Properties: {
-        ScheduleExpression: rule,
-        Targets: [
-          {
-            Arn: { 'Fn::GetAtt': [ scheduleLambda, 'Arn' ] },
-            Id: scheduleLambda,
+        AssumeRolePolicyDocument: {
+          Version: '2012-10-17',
+          Statement: [ {
+            Effect: 'Allow',
+            Principal: {
+              Service: 'scheduler.amazonaws.com',
+            },
+            Action: 'sts:AssumeRole',
+          } ],
+        },
+        Policies: [ {
+          PolicyName: 'InvokeLambdaPolicy',
+          PolicyDocument: {
+            Version: '2012-10-17',
+            Statement: [ {
+              Effect: 'Allow',
+              Action: 'lambda:InvokeFunction',
+              Resource: { 'Fn::GetAtt': [ scheduleLambda, 'Arn' ] },
+            } ],
           },
-        ],
+        } ],
       },
     }
 
-    // Wire the permission
-    template.Resources[schedulePermission] = {
-      Type: 'AWS::Lambda::Permission',
+    // Create the schedule using AWS::Scheduler::Schedule
+    template.Resources[scheduleEvent] = {
+      Type: 'AWS::Scheduler::Schedule',
       Properties: {
-        Action: 'lambda:InvokeFunction',
-        FunctionName: { Ref: scheduleLambda },
-        Principal: 'events.amazonaws.com',
-        SourceArn: { 'Fn::GetAtt': [ scheduleEvent, 'Arn' ] },
+        ScheduleExpression: rule,
+        FlexibleTimeWindow: {
+          Mode: 'OFF',
+        },
+        Target: {
+          Arn: { 'Fn::GetAtt': [ scheduleLambda, 'Arn' ] },
+          RoleArn: { 'Fn::GetAtt': [ scheduleRole, 'Arn' ] },
+        },
       },
+    }
+
+    // Add timezone if specified
+    if (timezone) {
+      template.Resources[scheduleEvent].Properties.ScheduleExpressionTimezone = timezone
     }
   })
 
